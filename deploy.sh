f81051263c468a4e997077b84b3844bb17b0ecdd
Alvarado <alvarado.programador@gmail.com>
2023-04-06 09:12:03 -0600

    deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-npm run docs:build
-cd docs/.vitepress/dist
-
-git add -A
-git commit -m 'deploy'
-git push -f https://github.com/Alvarad0/nextcloud-docs.git main:gh-pages cd -
